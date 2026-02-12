e44d13fdd05132bcf027bc63e88691fdcbbfa300
Pedro Lucas <pedroldima52@gmail.com>
2026-02-11 22:28:54 -0300

    fix

--- app/controllers/api/salons_controller.rb
+++ app/controllers/api/salons_controller.rb
@@ -2,4 +2,9 @@ class Api::SalonsController < ApplicationController
     def index
         render json: Salon.all
     end
+
+    # Get /salons/:id
+    def show
+        render json: Salon.find(params[:id])
+    end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://salao-rose-esmalteria-qtju.vercel.app"
+    # URL LOCAL
+    # origins "http://localhost:5173"
+    origins "http://localhost:5173"
 
     resource "*",
       headers: :any,
