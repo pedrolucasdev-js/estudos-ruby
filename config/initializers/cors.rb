f5cf14d9f28859717f19ca1639fb9dac07c08b30
Pedro Lucas <114819685+pedrolucasdev-js@users.noreply.github.com>
2026-02-23 15:46:06 -0300

    Update CORS origin for Vercel deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Backend Render
             "https://estudos-ruby-1.onrender.com",
     # Frontend Vercel
-            "https://salao-rose-esmalteria-spatenraivosos-projects.vercel.app"
+            "https://salao-rose-esmalteria.vercel.app"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
